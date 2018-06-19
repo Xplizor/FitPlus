16263d9e2c327d682df0421389e185820f722ea3
Nordin Jansen <nordinjansen0127@gmail.com>
2018-06-19 10:00:07 +0200

    Home griview done

--- app/src/main/java/com/example/njansen/fitplus/GridAdapterNjan.java
+++ app/src/main/java/com/example/njansen/fitplus/GridAdapterNjan.java
@@ -1,18 +1,21 @@
 package com.example.njansen.fitplus;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GridAdapterNjan extends BaseAdapter {
 
     private final Context mContext;
-    private final String[] books;
+    private final MainActivity.Button[] books;
 
     // 1
-    public GridAdapterNjan(Context context, String[] books) {
+    public GridAdapterNjan(Context context, MainActivity.Button[] books) {
         this.mContext = context;
         this.books = books;
     }
@@ -38,9 +41,24 @@ public class GridAdapterNjan extends BaseAdapter {
     // 5
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView dummyTextView = new TextView(mContext);
-        dummyTextView.setText(String.valueOf(position));
-        return dummyTextView;
+
+        final MainActivity.Button book = books[position];
+
+        // 2
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.custom_grid_item, null);
+        }
+
+        // 3
+        final ImageView imageView = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
+        final TextView nameTextView = (TextView)convertView.findViewById(R.id.tvTitle);
+
+        // 4
+        imageView.setImageResource(book.imageNjan);
+        nameTextView.setText(book.titleNjan);
+
+        return convertView;
     }
 
 }
--- app/src/main/java/com/example/njansen/fitplus/MainActivity.java
+++ app/src/main/java/com/example/njansen/fitplus/MainActivity.java
@@ -6,15 +6,28 @@ import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] strings = {"1", "2", "3", "4"};
+    private Button[] buttonsNjan = new Button[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonsNjan[0] = new Button("Oefeningen", R.drawable.ic_excersie);
+        buttonsNjan[1] = new Button("Instellingen", R.drawable.ic_settings);
+
         GridView gridView = (GridView)findViewById(R.id.gvButtonsNjan);
-        GridAdapterNjan gridAdapter = new GridAdapterNjan(this, strings);
+        GridAdapterNjan gridAdapter = new GridAdapterNjan(this, buttonsNjan);
         gridView.setAdapter(gridAdapter);
+    }
+
+    class Button {
+        public String titleNjan = "";
+        public int imageNjan = 0;
+
+        Button(String title, int image) {
+            titleNjan = title;
+            imageNjan = image;
+        }
     }
 }
